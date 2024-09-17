e504fabe505f61d39d9504e1d10163db208599c4
balta <baltaasanchez@gmail.com>
2024-09-16 23:39:44 -0300

    Add Models

---
+++ Ecommerce_Data_Access_Layer/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber   { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+
+
+    }
+}
