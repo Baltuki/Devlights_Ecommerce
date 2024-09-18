23fb128da618615984cc8174cb46432b228da33d
balta <baltaasanchez@gmail.com>
2024-09-17 21:20:48 -0300

    Add DbContext

---
+++ Ecommerce_Data_Access_Layer/Data/EcommerceDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Data_Access_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Data
+{
+    public class EcommerceDbContext : DbContext 
+    {
+        public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options) { }
+    
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Order> Order {  get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
